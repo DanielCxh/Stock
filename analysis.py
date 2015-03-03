64463e289ba83454e2326960dd387852b643de12
chenxiaohui <chenxiaohui@pset.suntec.net>
2015-03-03 18:52:29 +0800

    [CXH] Modify

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+import csv
 
 rootdir = r".\data"
 
@@ -33,6 +34,18 @@ class StockData:
 
     def getStockClose(self):
         return self.close
+
+    def getStockHigh(self):
+        return self.high
+
+    def getStockLow(self):
+        return self.low
+
+    def getStockVolume(self):
+        return self.volume
+
+    def getStockAdjClose(self):
+        return self.adjClose
     
 #--------------------------------------------------------------------#
 
@@ -46,47 +59,142 @@ def readFile(fileName):
 
     global allStock
 
+    if -1 == fileName.find(".csv", (len(fileName) - len(".csv")), len(fileName)):
+        return
+    else:
+        pass
+
     stock = []
     fs = open(rootdir + "\\" + fileName)
 
-    i = 0
-    for line in fs:
-        if 0 == 1:
-            i = 1
-            print "--------------"
-            continue
-        else:
-            data = line.split(",")
-            mystock = StockData(fileName, data[0], data[1], data[2], data[3], data[4].split(), data[5], data[6])
-            stock.append(mystock)
+    # Skip first line
+    fs.readline()
+
+    reader = csv.reader(fs)
+    for c1, c2, c3, c4, c5, c6, c7 in reader:
+        mystock = StockData(fileName, c1, c2, c3, c4, c5, c6, c7)
+        stock.append(mystock)
+        #print c7
     allStock.append(stock)
     
 
 def analysis(myStock):
     print "-----------------------"
-    total = 0
-    #print len(myStock)
-    for stock in myStock:
-       
-        print float(stock.getStockClose().pop()) + 1
-        #total = float(stock.getStockClose().pop())
 
-    print total
+    Evaluation = 0
+    
+    total = 0
+    s_min = 999
+    s_max = 0
+    
+    total_sixty = 0
+    s_min_sixty = 999
+    s_max_sixty = 0
+
+    total_tirty = 0
+    s_min_tirty = 999
+    s_max_tirty = 0
+
+    stockId = ""
+
+    max_money = 0
+    avr_money = 0
+    min_money = 0
+
+    counter = 0
+    SIXTY_DAY = 60
+    TIRTY_DAY = 30
+
+    for stock in myStock:
+        if stockId == "":
+            stockId = stock.getStockId()
+        else:
+            pass
+            
+        val = float(stock.getStockOpen())
+        total = total + val
+
+        # Get 60Day total
+        if counter < SIXTY_DAY:
+            total_sixty = total_sixty + val
+
+            if counter < TIRTY_DAY:
+                total_tirty = total_tirty + val
+            else:
+                pass
+        
+        else:
+            pass
+
+        # Get the min value
+        if s_min > val:
+            s_min = val
+
+            if counter < SIXTY_DAY:
+                s_min_sixty = val
+
+                if counter < TIRTY_DAY:
+                    s_min_tirty =  val
+                else:
+                    pass
+            
+            else:
+                pass
+        
+        else:
+            pass
+
+        # Get the max value
+        if s_max < val:
+            s_max = val
+
+            if counter < SIXTY_DAY:
+                s_max_sixty = val
+
+                if counter < TIRTY_DAY:
+                    s_max_tirty =  val
+                else:
+                    pass
+                
+            else:
+                pass
+        else:
+            pass
+
+        counter += 1
+
+    
+    print ">StockID = %s" % (stockId)
+    print ">Stock number = %d" % (len(myStock))
+    print ">Total        = %f" % (total)
+    print ">Average      = %f" % (total / len(myStock))
+
+    print ">Min          = %f" % (s_min)
+    print ">Max          = %f" % (s_max)
+    
+    print ">60 Day Total = %f" % (total_sixty)
+    print ">60 Day Avg   = %f" % (total_sixty / SIXTY_DAY)
+
+    print ">60 Day Min   = %f" % (s_min_sixty)
+    print ">60 Day Max   = %f" % (s_max_sixty)
+
+    print ">30 Day Total = %f" % (total_tirty)
+    print ">30 Day Avg   = %f" % (total_tirty / TIRTY_DAY)
+
+    print ">30 Day Min   = %f" % (s_min_tirty)
+    print ">30 Day Max   = %f" % (s_max_tirty)
+
 
 
 #--------------------------------------------------------------------#
 
 getFiles()
-#print (rootdir + "\\" + allFiles[0])
 
 for myFile in allFiles:
     readFile(myFile)
 
-#print allStock[0][1].getStockClose()
-analysis(allStock[0])
-#print allStock[0][1].getStockClose()
-
-
+for myStock in allStock:
+    analysis(myStock)
 
 # End
 exitProj = raw_input ('Enter any key to exit the console!')
--- getAllData.py
+++ getAllData.py
@@ -1,6 +1,9 @@
-# encoding:UTF-8
+# -*- encoding:UTF-8 -*-
 
+from Tkinter import *
+#import tkMessageBox
 import urllib2
+import threading
 
 #-----------------------------------------#
 STATE_ERROR = -1
@@ -8,10 +11,18 @@ STATE_NORMAL = 0
 
 DOWNLOAD_PATH = ""
 
+# Yahoo finance url
 yahooFinanceUrl = "http://table.finance.yahoo.com/table.csv?s="
 
+# Shanghai
 LOCATION_SH = ".ss"
+
+# Shenzheng
 LOCATION_SZ = ".sz"
+
+dataList = "f=snd1l1c6"
+
+#----------------------------------------#
 
 
 def loadFile(url):
@@ -19,40 +30,86 @@ def loadFile(url):
 	# Init the state as START_ERROR
 	state = STATE_ERROR
 	global fileName
+
+	
 	
 	try:
 	    f = urllib2.urlopen(url)
-	    
-	    # Set the file path to save the download files
-	    DOWNLOAD_PATH = "data/" + fileName + ".csv"
-	    
 	    state = STATE_NORMAL
+	    
 	except urllib2.HTTPError, e:
 		errLog = "[%s] open fail. Code = [%d]" % (url, e.code)
 		print errLog
 
 	if STATE_NORMAL == state :
+
+            # Set the file path to save the download files
+	    DOWNLOAD_PATH = "data/" + fileName + ".csv"
+	    
 	    with open(DOWNLOAD_PATH, "wb") as data:
 	    	data.write(f.read())
 	   	print "Loaded! " + fileName
 	else:
 	    pass
 
-i = 601988
 
-while i < 601990:
-	stockNum = "%06d%s" % (i, LOCATION_SH) 
-	url = yahooFinanceUrl + stockNum
-	
-	# Get the file name by split the url by '='
-	fileName = url.split("=")[1]
-	loadFile(url)
-	i = i + 1
-	#print url
+# Get all stock data from internet
+def getAllData(stockNum):
+
+        global fileName
+        
+        # Download start
+	print "Load Start!"
+
+        stockNum = "%s%s" % (stockNum, LOCATION_SH) 
+        url = yahooFinanceUrl + stockNum
+                
+        # Get the file name by split the url by '='
+        fileName = url.split("=")[1]
+        loadFile(url)
+        #print url
+
+#-----------------------------------------#
+
+#---------------CALL BACK----------------------#
+class myThread(threading.Thread):
+        def __init__(self, threadID, name, counter):
+                threading.Thread.__init__(self)
+                self.threadID = threadID
+                self.name = name
+                self.counter = counter
+
+        def run(self):
+                getAllData(enteyStockNum.get())
+
+                
+def onLoadBtnClicked():
+        #tkMessageBox.showinfo("hh","oo")
+        print ("Stock Number : %s") % (enteyStockNum.get())
+        if "" == enteyStockNum.get():
+                print "The stock number is empty!"
+        else:
+                loadStockFile = myThread(1, "Thread_loading", 1)
+                loadStockFile.start()
+
+#----------------SCREEN------------------------#
+
+mainMenu = Tk()
+mainMenu.title("Stock")
+# Set main screen as 600x400
+mainMenu.geometry('600x400')
+
+xMenu = Menu(mainMenu)
+
+enteyStockNum = Entry(mainMenu, text = "Stock number")
+enteyStockNum.pack()
+
+loadBtn = Button(mainMenu, text = "Load", command = onLoadBtnClicked)
+loadBtn.configure(width = 10, height = 2)
+loadBtn.pack()
 
 
-
-
+mainMenu.mainloop()
 
 # End
 exitProj = raw_input ('Enter any key to exit the console!')
